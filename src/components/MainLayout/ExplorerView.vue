d8cd04825f576e612dd45d8b12514e65f85f5753
JRC-Tojo <jrc.tojo@gmail.com>
2026-05-27 15:19:25 +0900

    ✨ Added the generate demo data btn

--- src/components/MainLayout/ExplorerView.vue
+++ src/components/MainLayout/ExplorerView.vue
@@ -4,6 +4,15 @@
   <!-- <ExpContainer v-for="container in containers" :container="container" :key="container.path" /> -->
 
   <ExpFile v-for="file in files" :key="file.id" :doc-id="file.id" :title="file.title" />
+
+  <q-btn
+    v-show="files.length === 0"
+    outline
+    label="Demo"
+    color="primary"
+    class="full-width q-my-sm"
+    @click="onCreateDemo"
+  />
 </template>
 
 <script setup lang="ts">
@@ -12,20 +21,30 @@ import type { DocumentMetadata } from 'src/models/schemas';
 import { ref } from 'vue';
 import { onMounted } from 'vue';
 import ExpFile from './Explorer/ExpFile.vue';
+import { createDemoData } from 'src/utils/appInitializer.js';
 
 const api = useBackendApi();
 
 const files = ref<DocumentMetadata[]>([]);
 // const containers = ref();
 
+async function loadDocs() {
+  const apiRes = await api.getAllDocuments();
+  if (apiRes.success) {
+    files.value = apiRes.data;
+  }
+}
+
+async function onCreateDemo() {
+  await createDemoData();
+  await loadDocs();
+}
+
 onMounted(async () => {
   // const apiRes = await api.getAllContainers();
   // if (apiRes.success) {
   //   containers.value = apiRes.data;
   // }
-  const apiRes = await api.getAllDocuments();
-  if (apiRes.success) {
-    files.value = apiRes.data;
-  }
+  await loadDocs();
 });
 </script>
