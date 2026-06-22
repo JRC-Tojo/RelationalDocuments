e0641e8bf75592c0aff6a9d0462502546f749da6
JRC-Tojo <274191739+JRC-Tojo@users.noreply.github.com>
2026-06-22 05:57:09 +0000

    🚨 Apply Prettier Change

--- src/utils/obj/objmap.ts
+++ src/utils/obj/objmap.ts
@@ -3,7 +3,7 @@ import { fromEntries, toEntries } from './obj';
 // objectに対してmapできるようにする
 export function objMap<K extends string, V, K2 extends string, V2>(
   object: { [key in K]: V },
-  func: (key: K, value: V, index: number) => [K2, V2]
+  func: (key: K, value: V, index: number) => [K2, V2],
 ): {
   [key in K2]: V2;
 } {
@@ -15,37 +15,31 @@ export function objMap<K extends string, V, K2 extends string, V2>(
 // objectに対してmapできるようにする
 export function objValueMap<K extends string, V, V2>(
   object: { [key in K]: V },
-  func: (value: V, key: K, index: number) => V2
+  func: (value: V, key: K, index: number) => V2,
 ): {
   [key in K]: V2;
 } {
   const entries = toEntries(object);
-  const mapped = entries.map(([key, value], index): [K, V2] => [
-    key,
-    func(value, key, index),
-  ]);
+  const mapped = entries.map(([key, value], index): [K, V2] => [key, func(value, key, index)]);
   return fromEntries(mapped);
 }
 
 // objectに対してmapできるようにする
 export function objKeyMap<K extends string, V, K2 extends string>(
   object: { [key in K]: V },
-  func: (key: K, value: V, index: number) => K2
+  func: (key: K, value: V, index: number) => K2,
 ): {
   [key in K2]: V;
 } {
   const entries = toEntries(object);
-  const mapped = entries.map(([key, value], index): [K2, V] => [
-    func(key, value, index),
-    value,
-  ]);
+  const mapped = entries.map(([key, value], index): [K2, V] => [func(key, value, index), value]);
   return fromEntries(mapped);
 }
 
 // objectに対してforEachできるようにする
 export function objEach<K extends string, V>(
   object: { [key in K]: V },
-  func: (key: K, value: V, index: number) => void
+  func: (key: K, value: V, index: number) => void,
 ): void {
   const entries = toEntries(object);
   entries.forEach(([key, value], index) => func(key, value, index));
@@ -54,7 +48,7 @@ export function objEach<K extends string, V>(
 // 非同期関数でmapする
 export async function asyncMap<T, U>(
   values: Readonly<T[]>,
-  func: (value: T, index: number) => Promise<U>
+  func: (value: T, index: number) => Promise<U>,
 ): Promise<U[]> {
   return await Promise.all(values.map(func));
 }
@@ -62,7 +56,7 @@ export async function asyncMap<T, U>(
 // 非同期関数でforEachする
 export async function asyncForEach<T>(
   values: T[],
-  func: (value: T, index: number) => Promise<void>
+  func: (value: T, index: number) => Promise<void>,
 ): Promise<void> {
   await Promise.all(values.map(func));
 }
